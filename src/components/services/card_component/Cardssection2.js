0b45de62b884eddad64f232d9fbc93cf49ae685d
hamid <hamidovicsaili.gmail.com>
2023-03-09 18:37:22 +0100

    creating the second section in the services area

--- src/components/services/card_component/Cardssection2.js
+++ src/components/services/card_component/Cardssection2.js
@@ -1,11 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 export default function Cardssection2(props) {
+
+  const [count,setcount]=useState(0)
+
+  useEffect(()=>{
+    const interval=setInterval(()=>{
+      if(count<props.number){
+        setcount(count+1)
+      }
+    },5);
+    return () => clearInterval(interval);
+  }, [count, props.number]);
+
+
+
+  
+
   return (
     
       <div className='col-xl-3 col-sm-6 pt-3'>
         <div className='services-section2-content'>
-          <h2 className='counter'>{props.number}</h2>
+          <h2 className='counter'>{count}</h2>
           <h5>{props.title}</h5>
           <p>{props.text}</p>
         </div>
